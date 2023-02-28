1d68f332b8c30e1bb1e6c4c892f7260d4180b51d
Abel Domínguez Morejón <temausdomo@gmail.com>
2023-02-28 10:37:45 +0100

    Adaptar para los dispositivos más pequeños y modificar un poco el Inicio

--- src/Inicio.svelte
+++ src/Inicio.svelte
@@ -1,5 +1,5 @@
 <div class="text-center mt-5">
     <h1>Librería Frontend</h1>
-    <h3>Backend en: <a href="https://libreriabackend.herokuapp.com/" target="_blank" rel="noreferrer">https://libreriabackend.herokuapp.com/</a></h3>
-    <h4>Código fuente en: <a href="https://github.com/Ciridae/libreriafrontend.git" target="_blank" rel="noreferrer">https://github.com/Ciridae/libreriafrontend.git</a></h4>
+    <h3 class="mt-5 mb-4">Backend en: <a href="https://libreriabackend.herokuapp.com/" target="_blank" rel="noreferrer">https://libreriabackend.herokuapp.com/</a></h3>
+    <h3>Código fuente en: <a href="https://github.com/Ciridae/libreriafrontend.git" target="_blank" rel="noreferrer">https://github.com/Ciridae/libreriafrontend.git</a></h3>
 </div>
